4f48a8b08de1c47ea4a2952d0aebd61f62999676
Arluc <arthur.urlic@gmail.com>
2022-11-21 14:00:18 +0100

    Added test.js

---
+++ test.js
@@ -0,0 +1,7 @@
+let friendsAtYourParty = 0
+
+while (friendsAtYourParty < 10){
+    console.log('Another friend arrived at your party')
+    friendsAtYourParty++
+}
+console.log('There are', friendsAtYourParty, 'friends at your party')
