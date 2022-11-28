7663e18ae57df272638446792cc1c417348b8d71
Arluc <arthur.urlic@gmail.com>
2022-11-28 10:54:53 +0100

    wip

--- test.js
+++ test.js
@@ -1,7 +1,30 @@
-let friendsAtYourParty = 0
+// let friendsAtYourParty = 0
 
-while (friendsAtYourParty < 10){
-    console.log('Another friend arrived at your party')
-    friendsAtYourParty++
-}
-console.log('There are', friendsAtYourParty, 'friends at your party')
+// while (friendsAtYourParty < 10){
+//     console.log('Another friend arrived at your party')
+//     friendsAtYourParty++
+// }
+// console.log('There are', friendsAtYourParty, 'friends at your party')
+
+const me = {
+    name: {
+      first: "Brian",
+      last: "Holt",
+    },
+    location: {
+      streetNumber: 500,
+      street: "Fakestreet",
+      city: "Seattle",
+      state: "WA",
+      zipCode: 55555,
+      country: "USA",
+    },
+    getAddress() {
+      return `${this.name.first} ${this.name.last}
+  ${this.location.streetNumber} ${this.location.street}
+  ${this.location.city}, ${this.location.state} ${this.location.zipCode}
+  ${this.location.country}`;
+    },
+  };
+  
+  console.log(me.getAddress());
